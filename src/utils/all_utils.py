53cec5366bff7c43c84af1e4c46b8c0cf05a00f9
Siddhant <siddhant99dei@gmail.com>
2021-10-07 15:06:55 +0530

    stage 02addeed

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -10,8 +10,12 @@ def read_yaml(path_to_yaml: str) -> dict:
 
     return content
 
-    
+
 def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
         print(f"directory is created at {dir_path}")
+
+def save_local_df(data, data_path,index_status=False):
+    data.to_csv(data_path, index=index_status)
+    print(f"data is saved at {data_path}")
